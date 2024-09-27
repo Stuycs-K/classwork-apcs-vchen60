3cfa6dae300174e80b7128ba45465f1730770aeb
Vincent Chen <chen.vincent.ny@gmail.com>
2024-09-27 10:57:17 -0400

    added more test cases for returnCopy

--- 2024-09-27/Main.java
+++ 2024-09-27/Main.java
@@ -1,12 +1,25 @@
 import java.util.*;
 public class Main {
   public static void main(String[] args) {
-    int[] j = new int[]{1,2,3,4,56,7,8};
+    int[] j = new int[]{1,2,3,4,5,6,7,8};
     int[] a = MyArrays.returnCopy(j);
-    a[3] = 100000;
-    System.out.println(j);
-    System.out.println(Arrays.toString(j));
-    System.out.println(a);
-    System.out.println(Arrays.toString(a));
+    a[7] = 100000;
+    System.out.println(j); //test address
+    System.out.println(Arrays.toString(j)); //test values inside
+    System.out.println(a); //test address
+    System.out.println(Arrays.toString(a)); //test values inside; should be different
+    // confirms that the function doesn't duplicate value, so following test cases will just check for identicalness
+
+    int[] k = new int[]{};
+    int[] l = new int[]{1010101010101}
+    int[] m = new int[]{3,6,9,12,15};
+    int[] n = new int[]{5,6,7,8,9,0};
+
+    System.out.println(Arrays.toString(MyArrays.returnCopy(k)));
+    System.out.println(Arrays.toString(MyArrays.returnCopy(l)));
+    System.out.println(Arrays.toString(MyArrays.returnCopy(m)));
+    System.out.println(Arrays.toString(MyArrays.returnCopy(n)));
+
+    
   }
 }
