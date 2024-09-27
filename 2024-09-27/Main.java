5f3b2511506a3e28c2c5375bb393f269a6cabadc
Vincent Chen <chen.vincent.ny@gmail.com>
2024-09-27 10:52:14 -0400

    made edits to tests to include Arrays.toString

--- 2024-09-27/Main.java
+++ 2024-09-27/Main.java
@@ -1,12 +1,12 @@
+import java.util.*;
 public class Main {
   public static void main(String[] args) {
-    System.out.println()
     int[] j = new int[]{1,2,3,4,56,7,8};
-    int[] a = MyArrays.returnCopy(int[] j);
-    j[3] = 100000;
+    int[] a = MyArrays.returnCopy(j);
+    a[3] = 100000;
     System.out.println(j);
-    System.out.println(j.toString());
+    System.out.println(Arrays.toString(j));
     System.out.println(a);
-    System.out.println(a.toString());
+    System.out.println(Arrays.toString(a));
   }
 }
