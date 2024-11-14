c6c167439e3981ec2ad75c1e8333fc8533c6b453
Vincent Chen <chen.vincent.ny@gmail.com>
2024-11-14 10:59:39 -0500

    added test cases and finished last 2 methods

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -29,13 +29,37 @@ public class ArrayListPractice {
 
 public static ArrayList<String> makeReversedList( ArrayList<String> original){
   //return a new ArrayList that is in the reversed order of the original.
-  
+    ArrayList<String> newArr = new ArrayList<String>();
+    for (int i = original.size() - 1; i >= 0; i--)
+    {
+      newArr.add(original.get(i));
+    }
+    return newArr;
+
 }
 
 public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
   //return a new ArrayList that has all values of a and b in alternating order that is:
   //a[0], b[0], a[1], b[1]...
   //If one list is longer than the other, just attach the remaining values to the end.
+  ArrayList<String> newArr = new ArrayList<String>();
+  if (a.size() <= b.size())
+  {
+  for(int i = 0; i < a.size(); i++)
+  {
+    newArr.add(a.get(i));
+    newArr.add(b.get(i));
+  }
+  for(int i = a.size(); i < b.size(); i++) {
+    newArr.add(b.get(i));
+  }
+  return newArr;
+
 }
+else {
+  return mixLists(b,a);
+}
+}
+
 
 }
--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -8,5 +8,22 @@ public static void main(String[] args) {
   ArrayList<String> newArr = (ArrayListPractice.createRandomArray(j));
   ArrayListPractice.replaceEmpty(newArr);
   System.out.println(newArr.toString());
+  System.out.println(ArrayListPractice.makeReversedList(newArr).toString());
+
+  int i = (int)(50*Math.random());
+
+  ArrayList<String> newArr1 = (ArrayListPractice.createRandomArray(j));
+  ArrayList<String> newArr2 = (ArrayListPractice.createRandomArray(i));
+  ArrayListPractice.replaceEmpty(newArr1);
+  ArrayListPractice.replaceEmpty(newArr2);
+
+    System.out.println("BEFORE");
+    System.out.println(newArr1.toString());
+  ArrayList<String> newArr3 = ArrayListPractice.makeReversedList(newArr1);
+
+    System.out.println("AFTER");
+  System.out.println(newArr3.toString());
+  System.out.println(ArrayListPractice.mixLists(newArr1, newArr2).toString());
+
 }
 }
