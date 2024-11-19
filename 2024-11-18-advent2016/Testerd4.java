aa49b2d7583b1fb24f9db47f51f285836bce2cc9
Vincent Chen <chen.vincent.ny@gmail.com>
2024-11-19 10:58:08 -0500

    problem 4 done (atrociously long)

--- 2024-11-18-advent2016/Testerd4.java
+++ 2024-11-18-advent2016/Testerd4.java
@@ -4,15 +4,16 @@ import java.io.FileNotFoundException;
 public class Testerd4 {
     public static void main(String[] args) {
 
-      
+
       System.out.println(codeA("inputTri.txt"));
+      codeB("inputTri.txt");
 
     }
 
     public static int codeA(String filename) {
       int[] arr = new int[26];  // Array to store frequency of characters (a-z)
         String result = "";
-        
+
       //CODE THAT SCANS THE FILE.
         int sum = 0;
         try {
@@ -24,10 +25,10 @@ public class Testerd4 {
             String line = sc.nextLine();
             int i = 0;
             while (i < line.length()) {
-              char c = line.charAt(i);  
-              if (Character.isLetter(c)) { 
-                  int index = c - 'a';  
-                  arr[index]++; 
+              char c = line.charAt(i);
+              if (Character.isLetter(c)) {
+                  int index = c - 'a';
+                  arr[index]++;
               }
               if(c=='[') {
                 break;
@@ -62,25 +63,56 @@ public class Testerd4 {
 
 
           }
-          
+
     catch (FileNotFoundException ex) {
       //File not found what should you do?
       System.out.println("File not found");
       return -1; //you can return from a void function just don't put a value.
     }
+  }
 
+public static String rot26(char x, int y) {
+    int z = x + y;
+    if(z>122) {
+      z-=26;
+    }
+    if(z>('z'+26)) {
+      return Character.toString ((char) z-26);
+    }
+    return Character.toString ((char) z);
+}
 
-    }}
-/*
-    public static int codeB(String filename) {
-      
+    public static void codeB(String filename) {
         try {
-         
-      } catch (FileNotFoundException ex) {
+          File file = new File(filename);
+          Scanner sc = new Scanner(file);
+
+          while(sc.hasNext()) {
+            String l = "";
+            String line = sc.nextLine();
+            String clean = line.replaceAll("\\D+","");
+            int o = Integer.parseInt(clean);
+            o = o % 26;
+            for(int i = 0; i < line.length();i++) {
+              if(Character.isLetter(line.charAt(i))){
+              l += rot26(line.charAt(i),o);
+            }
+            else if(line.charAt(i) == '-') {
+            l+= " ";
+          }
+          else if(line.charAt(i) == '[') {
+          l+= " ";
+        }
+            else {
+              l+= line.charAt(i);
+            }
+          }
+          System.out.println(l);
+      }} catch (FileNotFoundException ex) {
         //File not found what should you do?
         System.out.println("File not found");
-        return -1; //you can return from a void function just don't put a value.
+        return; //you can return from a void function just don't put a value.
       }
-      */
-  
-      
+    }
+
+    }
