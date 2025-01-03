d75eefeb9596722806e83535a8d74c0bd06f904a
Vincent Chen <chen.vincent.ny@gmail.com>
2025-01-03 10:55:27 -0500

    added the makeWords function

--- 2025-11-02/Main.java
+++ 2025-11-02/Main.java
@@ -1,8 +1,12 @@
 public class Main {
 public static void main(String[] args) {
-  int x = Integer.parseInt(args[0]);
-  System.out.println(fibonacci(x));//shortest in 10 sec is 46, also the largest we can do, 47 overflows
+  //int x = Integer.parseInt(args[0]);
+  //System.out.println(fibonacci(x));//shortest in 10 sec is 46, also the largest we can do, 47 overflows
   //largest we can go
+
+  makeWords(3,"","abcd");
+
+    makeWords(10,"","01");
 }
 
 public static int fibonacci(int n) {
@@ -12,5 +16,15 @@ public static int fibonacci(int n) {
   return fibonacci(n-1)+fibonacci(n-2);
 }
 
+public static void makeWords(int remaining, String partial, String alphabet) {
+  if(remaining == 0) {
+    System.out.println(partial);
+  }
+  else {
+    for(int i =0;i<alphabet.length();i++) {
+      makeWords(remaining-1, partial+alphabet.charAt(i), alphabet);
+    }
+  }
+}
 
 }
